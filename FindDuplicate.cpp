a0b5351d9a8b4bb6e31cca2e7bb7a9fa845decb0
Saiyam Jain <jimisierra2@gmail.com>
2022-06-05 11:44:24 +0530

    Find Duplicate in Array

---
+++ FindDuplicate.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int findDuplicate(vector<int> &arr, int n)
+{
+    sort(arr.begin(), arr.end());
+    for (int i = 0; i < n - 1; i++)
+    {
+        if (arr[i] == arr[i + 1])
+        {
+            return arr[i];
+        }
+    }
+    return -1;
+}
